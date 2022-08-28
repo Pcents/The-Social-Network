ad3671ff8f17160a7d3fb508b57608f477b359ca
James Plasencia <jamesplasencia@gmail.com>
2022-08-28 12:24:53 -0700

    delete and get single user working

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,9 +14,52 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "no user found" })
+          : res.json({
+              user,
+              // do we need to pull in thoughts?
+              // thought:await thought(req.params.UserId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
   createUser(req, res) {
     User.create(req.body)
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
+  },
+  deleteSingleUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res
+              .user(404)
+              .json({ message: "Can not delete what does not exist" })
+          : Thought.findOneAndUpdate(
+              { user: req.params.userId },
+              //   $pull operator removes an existing array and all instances of values
+              { $pull: { user: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({
+              message: "User deleted, but no thoughts found",
+            })
+          : res.json({ message: "User gone" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 
-const { getUsers, createUser } = require("../../controllers/userController");
+const {
+  getUsers,
+  createUser,
+  getSingleUser,
+  deleteSingleUser,
+} = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).delete(deleteSingleUser);
 
 module.exports = router;
